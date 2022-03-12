66f36ab5e7dcdd17078de5da7ba0e3bc41ac7226
ArtiomB5 <artiomb5@yandex.by>
2022-03-12 17:46:41 +0300

    old reducer removed

--- src/store/reposReducer.ts
+++ src/store/reposReducer.ts
@@ -32,68 +32,6 @@ export const {setRepos, setTotalCount, setIsSearching, sortByDate} = counterSlic
 
 export const reposReducer = counterSlice.reducer;
 
-// export const reposReducer = (
-//   state: StateType = initialState,
-//   action: ActionType
-// ) => {
-//   switch (action.type) {
-//     case "SET_REPOS":
-//       return { ...state, items: action.payload };
-//     case "SET_TOTAL_COUNT":
-//       return { ...state, totalCount: action.payload };
-//     case "SET_SEARCHING":
-//       return { ...state, isSearching: action.payload };
-//     case "SET_BY_DATE":
-//       const deepCopyItems = state.items.map((i: ItemType) => {
-//         return { ...i, owner: { ...i.owner } };
-//       });
-//       const filteredArray = deepCopyItems.sort((a, b) => {
-//         const dateA = new Date(a.created_at).valueOf();
-//         const dateB = new Date(b.created_at).valueOf();
-//         if (action.payload) {
-//           return dateA - dateB;
-//         } else {
-//           return dateB - dateA;
-//         }
-//       });
-//       return {
-//         ...state,
-//         items: filteredArray,
-//         sortHightLow: action.payload,
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
-// export const SetRepos = (payload: ItemType[]) => {
-//   return {
-//     type: "SET_REPOS",
-//     payload,
-//   } as const;
-// };
-
-// export const SetTotalCount = (payload: number) => {
-//   return {
-//     type: "SET_TOTAL_COUNT",
-//     payload,
-//   } as const;
-// };
-
-// export const SetIsSearching = (payload: boolean) => {
-//   return {
-//     type: "SET_SEARCHING",
-//     payload,
-//   } as const;
-// };
-
-// export const SortByDate = (payload: boolean) => {
-//   return {
-//     type: "SET_BY_DATE",
-//     payload,
-//   } as const;
-// };
-
 //thunks
 export const fetchRepos = (searchRequest: string) => {
   return (dispatch: Dispatch<ThunkDispatch>) => {
